1cc15615e1d6d8c24b359dab444017e174c687ea
RShubham kumar <115211090+rs1819-web@users.noreply.github.com>
2023-08-26 16:48:24 +0530

    file uploded

---
+++ script.js
@@ -0,0 +1,25 @@
+const endDate = "24 Feburary 2024 11:53 AM";
+
+document.querySelector('#end-date').innerHTML = endDate;
+
+const inputs = document.querySelectorAll("input");
+
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+
+    if(diff < 0) return;
+
+    inputs[0].value = Math.floor(diff / 3600 /24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
